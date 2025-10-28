04de56b18a7866d19c11ab82ecfb4a4ea771e7ee
Em Jones <emjonesdev@gmail.com>
2025-10-28 16:31:21 -0500

    docs: introduce authoring tooling- md syntax and grammar linting

--- init.sh
+++ init.sh
@@ -5,3 +5,8 @@ alias pnpm="corepack pnpm"
 alias pnpx="corepack pnpx"
 alias npm="corepack npm"
 alias npx="corepack npx"
+
+
+
+uv tool install specify-cli --from git+https://github.com/github/spec-kit.git --force
+vale sync
